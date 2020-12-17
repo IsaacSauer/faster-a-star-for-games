9f0d5dd9813a96010c4b18c09a06a727ada0ac81
Isaac Sauer <isaacsauer@hotmail.be>
2020-12-17 21:02:10 +0100

    Empty

---
+++ source/projects/App_Steering/SteeringBehaviors.h
@@ -0,0 +1,65 @@
+/*=============================================================================*/
+// Copyright 2017-2018 Elite Engine
+// Authors: Matthieu Delaere, Thomas Goussaert
+/*=============================================================================*/
+// SteeringBehaviors.h: SteeringBehaviors interface and different implementations
+/*=============================================================================*/
+#ifndef ELITE_STEERINGBEHAVIORS
+#define ELITE_STEERINGBEHAVIORS
+
+//-----------------------------------------------------------------
+// Includes & Forward Declarations
+//-----------------------------------------------------------------
+#include "SteeringHelpers.h"
+class SteeringAgent;
+using namespace Elite;
+
+#pragma region **ISTEERINGBEHAVIOR** (BASE)
+class ISteeringBehavior
+{
+public:
+	ISteeringBehavior() = default;
+	virtual ~ISteeringBehavior() = default;
+
+	virtual SteeringOutput CalculateSteering(float deltaT, SteeringAgent* pAgent) = 0;
+
+	//Seek Functions
+	void SetTarget(const TargetData& target) { m_Target = target; }
+
+	template<class T, typename std::enable_if<std::is_base_of<ISteeringBehavior, T>::value>::type* = nullptr>
+	T* As()
+	{ return static_cast<T*>(this); }
+
+protected:
+	TargetData m_Target;
+};
+#pragma endregion
+
+///////////////////////////////////////
+//SEEK
+//****
+class Seek : public ISteeringBehavior
+{
+public:
+	Seek() = default;
+	virtual ~Seek() = default;
+
+	//Seek Behaviour
+	SteeringOutput CalculateSteering(float deltaT, SteeringAgent* pAgent) override;
+};
+
+//////////////////////////
+//WANDER
+//******
+class Wander : public Seek
+{
+public:
+	Wander() = default;
+	virtual ~Wander() = default;
+
+	//Wander Behavior
+	SteeringOutput CalculateSteering(float deltaT, SteeringAgent* pAgent) override;
+};
+#endif
+
+
